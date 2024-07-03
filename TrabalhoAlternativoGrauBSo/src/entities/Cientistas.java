47534ee6f17eb7bc5449c870437f99f7b1147fd9
Carlos Eduardo Marchonatto <157914107+CarlosMRCT@users.noreply.github.com>
2024-07-03 17:29:49 -0300

    Update Cientistas.java
    att #1
    Adicionado tratamento para valor menor ou igual a zero na criação de novos processos.

--- TrabalhoAlternativoGrauBSo/src/entities/Cientistas.java
+++ TrabalhoAlternativoGrauBSo/src/entities/Cientistas.java
@@ -109,7 +109,11 @@ public class Cientistas implements Runnable {
                     if (decision.equalsIgnoreCase("y")) {
                         System.out.println("Digite a quantidade de novos processos criados:");
                         int sup = tc.nextInt();
-                        criarProcesso(sup);
+                        do {
+                    	   System.out.println("Digite a quantidade de novos processos criados:");
+                           sup = tc.nextInt();
+                        }while (sup <= 0);
+                       criarProcesso(sup);
                     }
 
                 }
