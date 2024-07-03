8719697a87c86bef2b87d1d4a4e48c259da9a225
Carlos Eduardo Marchonatto <157914107+CarlosMRCT@users.noreply.github.com>
2024-07-03 01:36:02 -0300

    Versão alternativa do trabalho

---
+++ TrabalhoAlternativoGrauBSo/src/application/Main.java
@@ -0,0 +1,21 @@
+package application;
+
+import entities.Cientistas;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+
+        Cientistas cientistas = new Cientistas();
+
+
+        Thread cientistasThread = new Thread(cientistas);
+
+        cientistasThread.start();
+
+        try {
+            cientistasThread.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
